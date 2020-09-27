b8ebe714279257e69d4163a6d8b5e870da704d9c
sunilrai <sunilrai7607@gmail.com>
2020-09-27 11:55:40 -0400

    Updates

---
+++ precache-manifest.234913a8e72652cf2e7d67109bf1e643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1404cf18736d145ba990dff9ac8c727d",
+    "url": "/react-sample-app/index.html"
+  },
+  {
+    "revision": "d4126180f099ad9b5848",
+    "url": "/react-sample-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "916f073fe6f0e483e2bd",
+    "url": "/react-sample-app/static/js/2.5afc43da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sample-app/static/js/2.5afc43da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4126180f099ad9b5848",
+    "url": "/react-sample-app/static/js/main.11f8758e.chunk.js"
+  },
+  {
+    "revision": "17e892ecf13f4e7960e6",
+    "url": "/react-sample-app/static/js/runtime-main.19f26b8f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sample-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
